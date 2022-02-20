503d7813e5bb450330b5b3dab3f5970d4e19b414
Lemmy Mwaura <lemminton@gmail.com>
2022-02-20 09:27:53 +0300

    created user class

---
+++ user.py
@@ -0,0 +1,24 @@
+class User:
+    number_of_users = 0
+    user_id = 0
+    user_list = []
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+        self.id = User.number_of_users + 1
+        User.add_user()
+
+    def save_user(self):
+        User.user_list.append(self)
+    
+    def delete_user(self):
+        User.user_list.remove(self)
+
+    @classmethod
+    def add_user(cls):
+        cls.number_of_users += 1
+    
+    @classmethod
+    def total_number_of_users(cls):
+        return cls.number_of_users
